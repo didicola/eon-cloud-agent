40237d42855b2dce85b3a209ec18a1f74671f2e8
didicola <160773725+didicola@users.noreply.github.com>
2026-08-07 14:57:40 +0000

    probe from termux

---
+++ commands/ubuntu_1786114659_relayprobe.cmd
@@ -0,0 +1,6 @@
+FROM: termux
+TIME: 2026-08-07 14:57:39 UTC
+CHANNEL: github-relay
+PRIORITY: HIGH
+
+CMD: echo GH_RELAY_ALIVE_1786114659 && hostname && whoami && date -u && ls /home/ricos 2>/dev/null | head -5
